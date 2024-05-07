762fe5e7013d8c7e312fb710feaaa4531ca70006
JimboT0 <james@origintime.co.za>
2024-05-07 18:48:45 +0200

    env to env.loc

---
+++ dist/static/index-be436b57-23b9010d.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-56aa5cc3-a3fc9133.js";import"./sanity-24881f39.js";import"./json-inspector-8fa109cd.js";export{m as DocumentPaneProvider,a as default};
