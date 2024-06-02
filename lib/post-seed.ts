76e82a6aa70fdf3a2ec94da12ef6ad4d5514235e
JimboT0 <james@origintime.co.za>
2024-06-02 14:36:17 +0200

    r

--- lib/post-seed.ts
+++ lib/post-seed.ts
@@ -21,11 +21,7 @@ export async function seedSanityData() {
   })
   await transaction.commit()
   await seedSanityImages()
-<<<<<<< HEAD
   // console.log("Sanity data seeded")
-=======
-  console.log("Sanity data seeded")
->>>>>>> main
 }
 
 async function seedSanityImages() {
